f8ad024dd0c95634bf9a7157fd1af906ff6900f1
Jiho <ruddbsghdus@naver.com>
2024-10-16 04:01:54 +0900

    feat: video 카테고리 별 조회 기능 완료

--- back-end/src/main/java/com/techie/backend/video/service/VideoService.java
+++ back-end/src/main/java/com/techie/backend/video/service/VideoService.java
@@ -1,28 +1,34 @@
 package com.techie.backend.video.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.techie.backend.video.domain.Category;
 import com.techie.backend.video.domain.Video;
+import com.techie.backend.video.dto.VideoResponse;
 import com.techie.backend.video.repository.VideoRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
+@Transactional(readOnly = true)
 public class VideoService {
     @Value("${youtube.api.key}")
     private String apiKey;
 
     private final RestClient restClient;
     private final VideoRepository videoRepository;
+    private final ObjectMapper objectMapper;
 
 
     public String getVideoIdsAsString(Category category) {
@@ -31,13 +37,13 @@ public class VideoService {
                 .collect(Collectors.joining(","));
     }
 
-    public String listVideos(Category category) {
+    public List<VideoResponse> fetchVideosByCategory(Category category) throws JsonProcessingException {
         String ids = getVideoIdsAsString(category);
         String url = UriComponentsBuilder.newInstance()
                 .scheme("https")
                 .host("www.googleapis.com")
                 .path("/youtube/v3/videos")
-                .queryParam("part", "snippet")
+                .queryParam("part", "snippet,contentDetails")
                 .queryParam("id", ids)
                 .queryParam("key", apiKey)
                 .build()
@@ -52,8 +58,26 @@ public class VideoService {
                 .toEntity(String.class);
 
 
-        return response.getBody();
+        return convertJsonToVideoDTO(response.getBody());
     }
 
+    public List<VideoResponse> convertJsonToVideoDTO(String jsonResponse) throws JsonProcessingException {
+        JsonNode rootNode = objectMapper.readTree(jsonResponse);
+        JsonNode itemsNode = rootNode.get("items");
+
+        List<VideoResponse> videoResponses = new ArrayList<>();
+        if (itemsNode.isArray()) {
+            for (JsonNode itemNode : itemsNode) {
+                JsonNode snippetNode = itemNode.get("snippet");
+                JsonNode contentDetailsNode = itemNode.get("contentDetails");
+                VideoResponse videoResponse = objectMapper.readValue(snippetNode.toPrettyString(), VideoResponse.class);
+                String duration = contentDetailsNode.get("duration").asText();
+                videoResponse.setDuration(duration);
+                videoResponses.add(videoResponse);
+            }
+        }
+
+        return videoResponses;
+    }
 
 }
