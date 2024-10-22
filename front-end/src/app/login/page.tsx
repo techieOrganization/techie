75e9222dd4048e1b1383a3965d590763b9a8df6e
hyeok9912 <bbqhdl@gmail.com>
2024-10-22 15:11:39 +0900

    feat:로그인UI구현

--- front-end/src/app/login/page.tsx
+++ front-end/src/app/login/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '@/styles/pages/login/login.scss';
 // interface User {
 //   id: string;
 //   nickname: string;
@@ -10,8 +10,28 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <div>
-      <h2>로그인 페이지</h2>
+    <div className="login-container">
+      <div className="title-wrapper">
+        <h2>TECHIE</h2>
+      </div>
+      <div className="input-wrapper">
+        <div className="email-wrapper">
+          <span>이메일</span>
+          <input type="text" />
+        </div>
+        <div className="password-wrapper">
+          <span>비밀번호</span>
+          <input type="password" />
+          <div className="span-wrapper">
+            <span>아이디 찾기</span>
+            <span>비밀번호 찾기</span>
+            <span>회원가입</span>
+          </div>
+        </div>
+      </div>
+      <div className="button-wrapper">
+        <button>로그인</button>
+      </div>
     </div>
   );
 };
