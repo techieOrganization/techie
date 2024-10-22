c2278b317febe4394e219763ca5386cb9d0b339d
hyeok9912 <bbqhdl@gmail.com>
2024-10-22 18:14:50 +0900

    feat:버튼Link추가

--- front-end/src/app/login/page.tsx
+++ front-end/src/app/login/page.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import Link from 'next/link';
 import '@/styles/pages/login/login.scss';
 // interface User {
 //   id: string;
@@ -9,6 +12,16 @@ import '@/styles/pages/login/login.scss';
 // }
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <div className="login-container">
       <div className="title-wrapper">
@@ -17,15 +30,21 @@ const Login = () => {
       <div className="input-wrapper">
         <div className="email-wrapper">
           <span>이메일</span>
-          <input type="text" />
+          <input type="text" onChange={onChangeEmail} value={email} />
         </div>
         <div className="password-wrapper">
           <span>비밀번호</span>
-          <input type="password" />
+          <input type="password" onChange={onChangePassword} value={password} />
           <div className="span-wrapper">
-            <span>아이디 찾기</span>
-            <span>비밀번호 찾기</span>
-            <span>회원가입</span>
+            <span>
+              <Link href="#">아이디 찾기</Link>
+            </span>
+            <span>
+              <Link href="#">비밀번호 찾기</Link>
+            </span>
+            <span>
+              <Link href={'/signup'}>회원가입</Link>
+            </span>
           </div>
         </div>
       </div>
