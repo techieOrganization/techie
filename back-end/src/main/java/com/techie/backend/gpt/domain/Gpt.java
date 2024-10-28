ff1b00b35d34bf9b87fd8b07e02717ab9c5d53f6
Donguk Kim <129242580+atoonatoo@users.noreply.github.com>
2024-10-28 12:35:12 +0900

    [BE] [#6] GPT 기능 추가 (#30)
    * feat: gpt 서비스 구현
    
    * chore: 절대 경로에 gitignore 추가
    
    * chore: gitignore .idea 추가
    
    * chore: gitignore 수정 및 Gpt 클래스 수정

---
+++ back-end/src/main/java/com/techie/backend/gpt/domain/Gpt.java
@@ -0,0 +1,41 @@
+package com.techie.backend.gpt.domain;
+
+import com.techie.backend.user.domain.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "gpt")
+public class Gpt {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String request;
+
+    @Column(nullable = false)
+    private String response;
+
+    @JoinColumn(name = "user_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    public void updateRequest(String request) {
+        this.request = request;
+    }
+
+    public void updateResponse(String response) {
+        this.response = response;
+    }
+
+    public void assignUser(User user) {
+        this.user = user;
+    }
+
+}
