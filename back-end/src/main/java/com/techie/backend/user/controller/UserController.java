cdf0bc3b5644fcb99ceb4960eeea42fd433e0bae
Donguk Kim <129242580+atoonatoo@users.noreply.github.com>
2024-10-30 11:59:51 +0900

    feat: 유저 조회, 수정, 탈퇴 기능 추가

--- back-end/src/main/java/com/techie/backend/user/controller/UserController.java
+++ back-end/src/main/java/com/techie/backend/user/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.techie.backend.user.controller;
 
 import com.techie.backend.user.dto.UserRequest;
+import com.techie.backend.user.dto.UserResponse;
 import com.techie.backend.user.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "사용자 인증 API", description = "사용자 인증 및 로그아웃 관련 API 엔드포인트")
@@ -14,10 +17,24 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
-
     @PostMapping("/register")
-    public ResponseEntity<Boolean> register(@RequestBody UserRequest userRequest) {
+    public ResponseEntity<Boolean> register(@RequestBody UserRequest.Register userRequest) {
         return ResponseEntity.ok(userService.register(userRequest));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponse.Information> getLoggedInUserInfo(@AuthenticationPrincipal UserDetails userDetails) {
+        return ResponseEntity.ok(userService.getLoggedInUser(userDetails));
+    }
+
+    @PutMapping("/me")
+    public ResponseEntity<Boolean> updateUserInfo(@AuthenticationPrincipal UserDetails userDetails, @RequestBody UserRequest.Update userRequest) {
+        return ResponseEntity.ok(userService.updateUser(userDetails, userRequest));
+    }
+
+    @DeleteMapping("/me")
+    public ResponseEntity<Boolean> deleteUserInfo(@AuthenticationPrincipal UserDetails userDetails) {
+        return ResponseEntity.ok(userService.deleteUser(userDetails));
     }
 
     @GetMapping("/authFail")
