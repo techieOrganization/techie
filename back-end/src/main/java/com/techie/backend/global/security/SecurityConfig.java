6c713dc535e5a34cfe5c44af710a2d2750abe811
Donguk Kim <129242580+atoonatoo@users.noreply.github.com>
2024-10-30 18:05:18 +0900

    chore: 중복 코드 제거

--- back-end/src/main/java/com/techie/backend/global/security/SecurityConfig.java
+++ back-end/src/main/java/com/techie/backend/global/security/SecurityConfig.java
@@ -29,7 +29,6 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorize) ->
                         authorize
                                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/swagger-resources/**", "/webjars/**").permitAll()
-                                .requestMatchers("/api/users/**").permitAll()
                                 .requestMatchers("/static/**").permitAll()
                                 .requestMatchers("/log.png").permitAll()
                                 .requestMatchers("/").permitAll()
