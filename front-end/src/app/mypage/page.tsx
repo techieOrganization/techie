130a114c4fd9e58071e3c0d3c2d2162d0e83c853
henny <kimhyein1105@gmail.com>
2024-10-31 13:46:28 +0900

    chroe: eslint 설정 변경

--- front-end/src/app/mypage/page.tsx
+++ front-end/src/app/mypage/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import React from 'react';
 import '@/styles/pages/mypage/mypage.scss';
@@ -16,11 +16,7 @@ const Mypage = () => {
             <label>
               <strong>닉네임</strong>
               <div className="modify_cont">
-                <input
-                  type="text"
-                  value="테스트 유저"
-                  readOnly
-                />
+                <input type="text" value="테스트 유저" readOnly />
                 <button>정보 수정</button>
               </div>
             </label>
@@ -28,27 +24,13 @@ const Mypage = () => {
           <div className="password_cont">
             <label>
               <strong>비밀번호</strong>
-              <input
-                type="password"
-                placeholder="현재 비밀번호 입력"
-                readOnly
-              />
-              <input
-                type="password"
-                placeholder="새 비밀번호 입력"
-                readOnly
-              />
-              <input
-                type="password"
-                placeholder="새 비밀번호 다시 입력"
-                readOnly
-              />
+              <input type="password" placeholder="현재 비밀번호 입력" readOnly />
+              <input type="password" placeholder="새 비밀번호 입력" readOnly />
+              <input type="password" placeholder="새 비밀번호 다시 입력" readOnly />
             </label>
           </div>
           <button className="pwd_modify_button">비밀번호 수정</button>
-          <button className="secession_button">
-            회원 탈퇴
-          </button>
+          <button className="secession_button">회원 탈퇴</button>
         </div>
       </div>
     </div>
