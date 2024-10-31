31a14ac446e8383cf619fa78b406b139aa6ed622
henny <kimhyein1105@gmail.com>
2024-10-31 13:42:50 +0900

    feat: 마이페이지 퍼블리싱

---
+++ front-end/src/app/mypage/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import React from 'react';
+import '@/styles/pages/mypage/mypage.scss';
+
+const Mypage = () => {
+  return (
+    <div className="mypage_container">
+      <div className="inner">
+        <h2 className="section_title">마이페이지</h2>
+        <div className="user_info_section">
+          <p className="email_cont">
+            <strong>이메일</strong> user@example.com
+          </p>
+          <div className="nickname_cont">
+            <label>
+              <strong>닉네임</strong>
+              <div className="modify_cont">
+                <input
+                  type="text"
+                  value="테스트 유저"
+                  readOnly
+                />
+                <button>정보 수정</button>
+              </div>
+            </label>
+          </div>
+          <div className="password_cont">
+            <label>
+              <strong>비밀번호</strong>
+              <input
+                type="password"
+                placeholder="현재 비밀번호 입력"
+                readOnly
+              />
+              <input
+                type="password"
+                placeholder="새 비밀번호 입력"
+                readOnly
+              />
+              <input
+                type="password"
+                placeholder="새 비밀번호 다시 입력"
+                readOnly
+              />
+            </label>
+          </div>
+          <button className="pwd_modify_button">비밀번호 수정</button>
+          <button className="secession_button">
+            회원 탈퇴
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Mypage;
