3405fad0ced993fa5c3995e3f56cb8c366c38059
Donguk Kim <129242580+atoonatoo@users.noreply.github.com>
2024-11-05 18:23:44 +0900

    [BE] 시큐리티 로그인 응답 JSON 타입으로 반환 문제 해결 (#61)
    * cofig 및 기본설정
    
    * 로그인, 회원가입, jwt 토큰 구현 성공
    
    * 로그인 구현 및 테스트 완료
    
    * 로그인 구현 및 테스트 완료
    
    * 중복 파일 삭제

--- back-end/src/main/java/com/techie/backend/user/controller/UserController.java
+++ back-end/src/main/java/com/techie/backend/user/controller/UserController.java
@@ -1,14 +1,20 @@
 package com.techie.backend.user.controller;
 
+import com.techie.backend.global.security.UserDetailsCustom;
+import com.techie.backend.user.domain.User;
 import com.techie.backend.user.dto.UserRequest;
 import com.techie.backend.user.dto.UserResponse;
 import com.techie.backend.user.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @Tag(name = "사용자 인증 API", description = "사용자 인증 및 로그아웃 관련 API 엔드포인트")
 @RequestMapping("/api/users")
@@ -18,35 +24,20 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/register")
-    public ResponseEntity<UserResponse.Information> register(@RequestBody UserRequest.Register userRequest) {
-        UserResponse.Information userInfo = userService.register(userRequest);
-        return ResponseEntity.ok(userInfo);
-    }
-
-    @GetMapping("/me")
-    public ResponseEntity<UserResponse.Information> getLoggedInUserInfo(@AuthenticationPrincipal UserDetails userDetails) {
-        return ResponseEntity.ok(userService.getLoggedInUser(userDetails));
+    @PostMapping("/join")
+    public ResponseEntity<Boolean> register(@RequestBody UserRequest.Register request) {
+        return ResponseEntity.ok(userService.joinProcess(request));
     }
 
     @PutMapping("/me")
-    public ResponseEntity<Boolean> updateUserInfo(@AuthenticationPrincipal UserDetails userDetails, @RequestBody UserRequest.Update userRequest) {
-        return ResponseEntity.ok(userService.updateUser(userDetails, userRequest));
+    public ResponseEntity<Boolean> updateUser(@AuthenticationPrincipal UserDetailsCustom userDetails, @RequestBody UserRequest.Update request) {
+        return ResponseEntity.ok(userService.updateUser(userDetails.getUsername(), request));
     }
 
     @DeleteMapping("/me")
-    public ResponseEntity<Boolean> deleteUserInfo(@AuthenticationPrincipal UserDetails userDetails) {
-        return ResponseEntity.ok(userService.deleteUser(userDetails));
+    public ResponseEntity<Boolean> deleteUser(@AuthenticationPrincipal UserDetailsCustom userDetails) {
+        return ResponseEntity.ok(userService.deleteUser(userDetails.getUsername()));
     }
 
-    @GetMapping("/authFail")
-    public ResponseEntity<String> authFail() {
-        return ResponseEntity.status(401).body("logIn Failed");
-    }
-
-    @GetMapping("logOutSuccess")
-    public ResponseEntity<String> logOutSuccess() {
-        return ResponseEntity.ok("Logged out successfully");
-    }
 
 }
--- back-end/src/main/java/com/techie/backend/user/service/UserServiceImpl.java
+++ back-end/src/main/java/com/techie/backend/user/service/UserServiceImpl.java
@@ -1,93 +1,61 @@
 package com.techie.backend.user.service;
 
-import com.techie.backend.global.exception.user.InvalidEmailFormatException;
-import com.techie.backend.global.exception.user.UserAlreadyExistsException;
+import com.techie.backend.global.security.JWTUtil;
 import com.techie.backend.user.domain.User;
 import com.techie.backend.user.dto.UserRequest;
-import com.techie.backend.user.dto.UserResponse;
 import com.techie.backend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final ModelMapper modelMapper;
-    private static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile(
-            "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$"
-    );
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final JWTUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
 
     @Override
-    public UserResponse.Information register(UserRequest.Register userRequest) {
+    public Boolean joinProcess(UserRequest.Register request) {
+        String email = request.getEmail();
+        String password = request.getPassword();
+        String nickname = request.getNickname();
 
-        if (!VALID_EMAIL_ADDRESS_REGEX.matcher(userRequest.getEmail()).matches()) {
-            throw new InvalidEmailFormatException();
+        Boolean iseExist = userRepository.existsByEmail(email);
+        if (iseExist) {
+            return false;
+
+        } else {
+            User data = User.builder()
+                    .email(email)
+                    .password(bCryptPasswordEncoder.encode(password))
+                    .nickname(nickname)
+                    .role("ROLE_ADMIN")
+                    .build();
+            userRepository.save(data);
+
+            return true;
         }
-
-        if (userRepository.findByEmail(userRequest.getEmail()).isPresent()) {
-            throw new UserAlreadyExistsException();
-        }
-
-        User user = User.builder()
-                .email(userRequest.getEmail())
-                .password(passwordEncoder.encode(userRequest.getPassword()))
-                .nickname(userRequest.getNickname())
-                .build();
-
-        userRepository.save(user);
-
-        // 저장된 User 엔티티를 UserResponse.Information DTO로 변환하여 반환
-        return UserResponse.Information.builder()
-                .email(user.getEmail())
-                .nickname(user.getNickname())
-                .createdDate(user.getCreatedDate())
-                .modifiedDate(user.getModifiedDate())
-                .build();
     }
 
-    @Override
-    public UserResponse.Information getLoggedInUser(UserDetails userDetails) {
-        String email = userDetails.getUsername();
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-        return modelMapper.map(user, UserResponse.Information.class);
-    }
 
     @Override
-    public Boolean updateUser(UserDetails userDetails, UserRequest.Update updateUserRequest) {
-        String email = userDetails.getUsername();
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-        String newEmail = updateUserRequest.getEmail() != null && VALID_EMAIL_ADDRESS_REGEX.matcher(updateUserRequest.getEmail()).matches()
-                ? updateUserRequest.getEmail() : user.getEmail();
-
-        String newNickname = updateUserRequest.getNickname() != null ? updateUserRequest.getNickname() : user.getNickname();
-
-        String newPassword = user.getPassword();
-        if (updateUserRequest.getCurrentPassword() != null && updateUserRequest.getNewPassword() != null) {
-            if (!passwordEncoder.matches(updateUserRequest.getCurrentPassword(), user.getPassword())) {
-                throw new IllegalArgumentException("Current password does not match password");
-            }
-            newPassword = passwordEncoder.encode(updateUserRequest.getNewPassword());
+    public Boolean updateUser(String email, UserRequest.Update request) {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            return false;
         }
-
         User updatedUser = User.builder()
-                .email(newEmail)
-                .password(newPassword)
-                .nickname(newNickname)
-                .createdDate(user.getCreatedDate()) // 기존 생성일 유지
-                .modifiedDate(LocalDateTime.now())  // 수정일 갱신
+                .id(user.getId())
+                .email(user.getEmail())
+                .nickname(request.getNickname() != null ? request.getNickname() : user.getNickname())
+                .password(request.getNewPassword() != null ? bCryptPasswordEncoder.encode(request.getNewPassword()) : user.getPassword())
+                .role(user.getRole())
                 .build();
 
         userRepository.save(updatedUser);
@@ -95,10 +63,14 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public Boolean deleteUser(UserDetails userDetails) {
-        String email = userDetails.getUsername();
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    public Boolean deleteUser(String email) {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            return false;
+        }
         userRepository.delete(user);
         return true;
     }
+
+
 }
