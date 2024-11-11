483c8ac7d194747892870b3cb08a623bf8363308
Donguk Kim <129242580+atoonatoo@users.noreply.github.com>
2024-11-12 01:04:22 +0900

    chore: 엔티티 실수 수정

--- back-end/src/main/java/com/techie/backend/gpt/domain/Gpt.java
+++ back-end/src/main/java/com/techie/backend/gpt/domain/Gpt.java
@@ -22,6 +22,7 @@ public class Gpt {
     private String response;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     public void updateRequest(String request) {
