12a2d3714e54dbd64f3050908f4970e1e8ac1973
henny <kimhyein1105@gmail.com>
2024-11-12 20:35:09 +0900

    chore: eslint 설정 값 변경

--- front-end/src/components/ClientProvider.tsx
+++ front-end/src/components/ClientProvider.tsx
@@ -2,6 +2,7 @@
 'use client'; // 클라이언트 컴포넌트로 설정
 
 import { Provider } from 'react-redux';
+
 import store from '@/redux/store';
 
 const ClientProvider = ({ children }: { children: React.ReactNode }) => {
