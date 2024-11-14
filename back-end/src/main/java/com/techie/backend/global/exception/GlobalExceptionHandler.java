94ab13c75b0158623f5b04008075227e2586b0bc
Donguk Kim <129242580+atoonatoo@users.noreply.github.com>
2024-11-14 15:11:30 +0900

    refactor: 유효성 검사 front로부터 예외처리 메세지 전달되도록 (#88)

--- back-end/src/main/java/com/techie/backend/global/exception/GlobalExceptionHandler.java
+++ back-end/src/main/java/com/techie/backend/global/exception/GlobalExceptionHandler.java
@@ -15,44 +15,56 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleUserNotFoundException(UserNotFoundException ex) {
-        Map<String, String> errorResponse = new HashMap<>();
-        errorResponse.put("error", ex.getExceptionType().getMessage());
+    public ResponseEntity<Map<String, Object>> handleUserNotFoundException(UserNotFoundException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("message", ex.getExceptionType().getMessage());
+        errorResponse.put("status", ex.getExceptionType().getStatus().value());
+
         return new ResponseEntity<>(errorResponse, ex.getExceptionType().getStatus());
     }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<Map<String, String>> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
-        Map<String, String> errorResponse = new HashMap<>();
-        errorResponse.put("error", ex.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    public ResponseEntity<Map<String, Object>> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("message", ex.getExceptionType().getMessage());
+        errorResponse.put("status", ex.getExceptionType().getStatus().value());
+
+        return new ResponseEntity<>(errorResponse, ex.getExceptionType().getStatus());
     }
 
     @ExceptionHandler(InvalidEmailFormatException.class)
-    public ResponseEntity<Map<String, String>> handleInvalidEmailFormatException(InvalidEmailFormatException ex) {
-        Map<String, String> errorResponse = new HashMap<>();
-        errorResponse.put("error", ex.getExceptionType().getMessage());
+    public ResponseEntity<Map<String, Object>> handleInvalidEmailFormatException(InvalidEmailFormatException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("message", ex.getExceptionType().getMessage());
+        errorResponse.put("status", ex.getExceptionType().getStatus().value());
+
         return new ResponseEntity<>(errorResponse, ex.getExceptionType().getStatus());
     }
 
     @ExceptionHandler(EmptyFieldException.class)
-    public ResponseEntity<Map<String, String>> handleEmptyFieldException(EmptyFieldException ex) {
-        Map<String, String> errorResponse = new HashMap<>();
-        errorResponse.put("error", ex.getExceptionType().getMessage());
+    public ResponseEntity<Map<String, Object>> handleEmptyFieldException(EmptyFieldException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("message", ex.getExceptionType().getMessage());
+        errorResponse.put("status", ex.getExceptionType().getStatus().value());
+
         return new ResponseEntity<>(errorResponse, ex.getExceptionType().getStatus());
     }
 
     @ExceptionHandler(PasswordTooShortException.class)
-    public ResponseEntity<Map<String, String>> handlePasswordTooShortException(PasswordTooShortException ex) {
-        Map<String, String> errorResponse = new HashMap<>();
-        errorResponse.put("error", ex.getExceptionType().getMessage());
+    public ResponseEntity<Map<String, Object>> handlePasswordTooShortException(PasswordTooShortException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("message", ex.getExceptionType().getMessage());
+        errorResponse.put("status", ex.getExceptionType().getStatus().value());
+
         return new ResponseEntity<>(errorResponse, ex.getExceptionType().getStatus());
     }
 
     @ExceptionHandler(PasswordMismatchException.class)
-    public ResponseEntity<Map<String, String>> handlePasswordMismatchException(PasswordMismatchException ex) {
-        Map<String, String> errorResponse = new HashMap<>();
-        errorResponse.put("error", ex.getMessage());
+    public ResponseEntity<Map<String, Object>> handlePasswordMismatchException(PasswordMismatchException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("message", ex.getExceptionType().getMessage());
+        errorResponse.put("status", ex.getExceptionType().getStatus().value());
+
         return new ResponseEntity<>(errorResponse, ex.getExceptionType().getStatus());
     }
 }
