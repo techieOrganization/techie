c58ea0278dce00625e9aec189026806ed4edc8a2
kimhyein <70068498+henny1105@users.noreply.github.com>
2024-11-18 15:33:48 +0900

    [FE] [#101] 유튜브 동영상 리스트 백엔드 연결 (#102)
    * feat: 유튜브 동영상 백엔드 연결
    
    * feat: 클라우드 아이콘 추가

--- front-end/src/app/api/videoAPI.ts
+++ front-end/src/app/api/videoAPI.ts
@@ -2,15 +2,17 @@ import { Video } from '@/types/video';
 
 export const fetchVideosByCategory = async (category: string): Promise<Video[]> => {
   try {
-    const response = await fetch(`/assets/json/videos.json`); // 실제 백엔드가 들어올 api 자리
+    const url = category === 'all' ? '/api/videos?query=' : `/api/videos/${category}`;
+
+    const response = await fetch(url);
+
     if (!response.ok) {
       throw new Error(`Failed to fetch videos for category: ${category}`);
     }
-    const data: Video[] = await response.json();
 
-    return category === 'all' ? data : data.filter((video) => video.category === category);
+    return await response.json();
   } catch (error) {
-    console.error(error);
+    console.error('Error fetching videos:', error);
     return [];
   }
 };
--- front-end/src/data/vidListData.js
+++ front-end/src/data/vidListData.js
@@ -1,14 +1,15 @@
 const vidListData = [
   { id: 'all', title: '전체', img: '/assets/images/main/dev_10.png' },
-  { id: 'language', title: '언어', img: '/assets/images/main/dev_01.png' },
-  { id: 'gameDev', title: '게임개발', img: '/assets/images/main/dev_02.png' },
-  { id: 'backend', title: '백엔드', img: '/assets/images/main/dev_03.png' },
-  { id: 'mobile', title: '모바일', img: '/assets/images/main/dev_04.png' },
-  { id: 'frontend', title: '프론트엔드', img: '/assets/images/main/dev_05.png' },
-  { id: 'data', title: '데이터', img: '/assets/images/main/dev_06.png' },
-  { id: 'ai', title: '인공지능', img: '/assets/images/main/dev_07.png' },
-  { id: 'security', title: '보안', img: '/assets/images/main/dev_08.png' },
-  { id: 'cs', title: 'CS', img: '/assets/images/main/dev_09.png' },
+  { id: 'LANG', title: '언어', img: '/assets/images/main/dev_01.png' },
+  { id: 'GAME', title: '게임개발', img: '/assets/images/main/dev_02.png' },
+  { id: 'BACK', title: '백엔드', img: '/assets/images/main/dev_03.png' },
+  { id: 'MOBILE', title: '모바일', img: '/assets/images/main/dev_04.png' },
+  { id: 'FRONT', title: '프론트엔드', img: '/assets/images/main/dev_05.png' },
+  { id: 'DATA', title: '데이터', img: '/assets/images/main/dev_06.png' },
+  { id: 'AI', title: '인공지능', img: '/assets/images/main/dev_07.png' },
+  { id: 'SEC', title: '보안', img: '/assets/images/main/dev_08.png' },
+  { id: 'CS', title: 'CS', img: '/assets/images/main/dev_09.png' },
+  { id: 'CLOUD', title: '클라우드', img: '/assets/images/main/dev_11.png' },
 ];
 
 export default vidListData;
