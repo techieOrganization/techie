1d2936f1e26d0c8000194001ca8f31e99aef0a8a
henny <kimhyein1105@gmail.com>
2024-11-28 22:16:10 +0900

    chore: 일부 주석 삭제

--- front-end/src/components/playlist/TeacherPlaylist.tsx
+++ front-end/src/components/playlist/TeacherPlaylist.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation'; // Next.js 라우터 사용
+import { useRouter, useSearchParams } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useQuery } from '@tanstack/react-query';
