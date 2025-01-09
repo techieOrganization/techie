f2786fc57e29b8a5e4ac52ed11ce7e5adf6357d8
henny <kimhyein1105@gmail.com>
2025-01-10 00:40:27 +0900

    chore: eslint 오류 수정

--- front-end/src/components/playlist/CategoryPlaylist.tsx
+++ front-end/src/components/playlist/CategoryPlaylist.tsx
@@ -231,21 +231,21 @@ const CategoryPlaylist: React.FC<CategoryPlaylistProps> = ({ category: initialCa
 
   return (
     <div className="playlists_container">
-      <div className='dev_list_cont'>
+      <div className="dev_list_cont">
         <ul className="dev_list">
-        {vidListData.map((tab) => (
-          <li key={tab.id} className={tab.id === category ? 'active' : ''}>
-            <button
-              type="button"
-              onClick={() => handleCategoryClick(tab.id)}
-              disabled={tab.id === category}
-            >
-              <Image src={tab.img} alt={tab.title} width={40} height={40} />
-              <span>{tab.title}</span>
-            </button>
-          </li>
-        ))}
-      </ul>
+          {vidListData.map((tab) => (
+            <li key={tab.id} className={tab.id === category ? 'active' : ''}>
+              <button
+                type="button"
+                onClick={() => handleCategoryClick(tab.id)}
+                disabled={tab.id === category}
+              >
+                <Image src={tab.img} alt={tab.title} width={40} height={40} />
+                <span>{tab.title}</span>
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
       <div className="video_list_cont">
         <div className="inner">
