793dc3b2a04f200947b54599084e2341bae259a4
Jiho001 <ruddbsghdus@naver.com>
2025-01-21 01:20:16 +0900

    feat:게시물 작성 createPost 완료

---
+++ back-end/src/main/java/com/techie/backend/global/mapper/PostMapper.java
@@ -0,0 +1,24 @@
+package com.techie.backend.global.mapper;
+
+import com.techie.backend.board.domain.Post;
+import com.techie.backend.board.dto.PostRequest;
+import com.techie.backend.board.dto.PostResponse;
+import com.techie.backend.user.domain.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostMapper {
+    public Post toEntity(PostRequest postRequest, User user) {
+        return Post.builder()
+                .title(postRequest.getTitle())
+                .content(postRequest.getContent())
+                .category(postRequest.getCategory())
+                .user(user)
+                .build();
+    }
+
+    public PostResponse toDto(Post post) {
+        return new PostResponse(post);
+    }
+
+}
