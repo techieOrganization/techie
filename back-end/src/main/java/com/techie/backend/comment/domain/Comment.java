aadd41f7622c9b02056b08a19535d48728151b50
Donguk Kim <129242580+atoonatoo@users.noreply.github.com>
2025-01-24 16:20:18 +0900

    feat: Entity 추가

---
+++ back-end/src/main/java/com/techie/backend/comment/domain/Comment.java
@@ -0,0 +1,31 @@
+package com.techie.backend.comment.domain;
+
+import com.techie.backend.board.domain.Post;
+import com.techie.backend.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "comment")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}
