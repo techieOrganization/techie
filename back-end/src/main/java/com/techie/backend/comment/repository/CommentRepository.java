2488d98987fcc8d93251a219491b770863127203
Donguk Kim <129242580+atoonatoo@users.noreply.github.com>
2025-01-24 16:32:50 +0900

    feat: repository 추가

---
+++ back-end/src/main/java/com/techie/backend/comment/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package com.techie.backend.comment.repository;
+
+import com.techie.backend.board.domain.Post;
+import com.techie.backend.comment.domain.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPost(Post post);
+}
