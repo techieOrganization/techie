ccf21c403c4b428e59a4f47a7709a11e3e9142bb
혁 <163282490+hyeok9912@users.noreply.github.com>
2025-01-28 11:16:47 +0900

    [FE][#91] 챗봇 ui 수정 (#211)
    * refector: 챗봇 ui 수정
    
    * chore: eslist 오류 수정
    
    * chore:챗봇 위치 수정
    
    * chore:eslint 수정

--- front-end/src/app/@isLoginChatbot/page.tsx
+++ front-end/src/app/@isLoginChatbot/page.tsx
@@ -3,15 +3,19 @@ import { useEffect, useState } from 'react';
 import '@/styles/pages/chatbot/chatbot.scss';
 import fetchChatBot from '../api/chatBotApi';
 import Cookies from 'js-cookie';
+// import { useSelector } from 'react-redux';
+// import { RootState } from '@/redux/store';
 import { devConsoleError } from '@/utils/logger';
 
 const Chatbot = () => {
   const [position, setPosition] = useState({ x: 1850, y: 1000 });
   const [isOpen, setIsOpen] = useState(false);
   const [textarea, setTextarea] = useState('');
-  const [gptResponse, setGptResponse] = useState('');
   const [loading, setLoading] = useState(false);
+  const [gptlog, setGptlog] = useState<{ user: string; bot: string }[]>([]);
   const MOVE_THRESHOLD = 10;
+
+  // const userInfo = useSelector((state: RootState) => state.user.userInfo);
 
   const toggleTextArea = () => {
     setIsOpen((prev) => !prev);
@@ -22,19 +26,7 @@ const Chatbot = () => {
     const xOffset = 90;
     const handleResize = () => {
       const { innerHeight, innerWidth } = window;
-
-      if (innerWidth > 767) {
-        setPosition({ x: innerWidth - xOffset, y: innerHeight - yOffset });
-      }
-      if (innerWidth < 767) {
-        setPosition({ x: innerWidth - xOffset + 20, y: innerHeight - yOffset + 10 });
-      }
-      if (innerWidth < 500) {
-        setPosition({ x: innerWidth - xOffset + 40, y: innerHeight - yOffset + 20 });
-      }
-      if (innerWidth < 330) {
-        setPosition({ x: innerWidth - xOffset + 60, y: innerHeight - yOffset + 25 });
-      }
+      setPosition({ x: innerWidth - xOffset, y: innerHeight - yOffset });
     };
 
     // 초기 호출
@@ -48,7 +40,6 @@ const Chatbot = () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
-
   const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
     const offsetX = e.clientX - position.x;
     const offsetY = e.clientY - position.y;
@@ -90,8 +81,17 @@ const Chatbot = () => {
     if (!token) return;
     try {
       setLoading(true);
+
+      const newLogEntry = { user: textarea, bot: '' };
+      setGptlog((prev) => [...prev, newLogEntry]);
       const apiResponse = await fetchChatBot({ request: textarea, token: token });
-      typeResponse(apiResponse.response);
+      // typeResponse(apiResponse.response);
+
+      setGptlog((prev) =>
+        prev.map((log, index) =>
+          index === prev.length - 1 ? { ...log, bot: apiResponse.response } : log,
+        ),
+      );
       setTextarea('');
     } catch (error) {
       devConsoleError('함수요청 오류', error);
@@ -100,18 +100,18 @@ const Chatbot = () => {
     }
   };
 
-  const typeResponse = (text: string) => {
-    setGptResponse('');
-    let index = -1;
-    const interval = setInterval(() => {
-      if (index < text.length - 1) {
-        setGptResponse((prev) => prev + text[index]);
-        index++;
-      } else {
-        clearInterval(interval);
-      }
-    }, 50);
-  };
+  // const typeResponse = (text: string) => {
+  //   setGptResponse('');
+  //   let index = -1;
+  //   const interval = setInterval(() => {
+  //     if (index < text.length - 1) {
+  //       setGptResponse((prev) => prev + text[index]);
+  //       index++;
+  //     } else {
+  //       clearInterval(interval);
+  //     }
+  //   }, 50);
+  // };
 
   const keyDownEnter = (e: React.KeyboardEvent) => {
     if (e.key === 'Enter' && !e.shiftKey) {
@@ -129,10 +129,23 @@ const Chatbot = () => {
       <div className="icon">💬</div>
       <div
         className={`chatbot-content_login ${isOpen ? 'isOpen' : ''}`}
-        style={{ right: 50, bottom: 40, position: 'absolute' }}
+        style={{ left: position.x - 580, top: position.y - 400, position: 'fixed' }}
       >
         <div className="chatbot-response" onMouseDown={(e) => e.stopPropagation()}>
-          <p>{loading ? '응답을 받아오는 중입니다...' : gptResponse}</p>
+          <div className="log-container">
+            {gptlog.map((log, index) => (
+              <div className="chatbot-log" key={`${log.user}-${index}`}>
+                {/* {userInfo?.nickname} */}
+                <span className="user-req">{log.user}</span>
+                Techie
+                <span className="bot-res">
+                  {loading && index === gptlog.length - 1
+                    ? 'Techie 가 답변을 준비중이에요!..'
+                    : log.bot}
+                </span>
+              </div>
+            ))}
+          </div>
         </div>
         <textarea
           value={textarea}
