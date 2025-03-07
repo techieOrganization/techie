70fbd92d962bb9828cd06e62670d1af39f0740fb
Jiho <ruddbsghdus@naver.com>
2025-03-07 15:19:40 +0900

    feat: OAuth2UserService 커스텀

--- back-end/src/main/java/com/techie/backend/oauth2/user/OAuth2UserInfoFactory.java
+++ back-end/src/main/java/com/techie/backend/oauth2/user/OAuth2UserInfoFactory.java
@@ -1,4 +1,17 @@
 package com.techie.backend.oauth2.user;
 
+import com.techie.backend.global.exception.user.OAuth2AuthenticationProcessingException;
+
+import java.util.Map;
+
 public class OAuth2UserInfoFactory {
+    public static OAuth2UserInfo getOAuth2UserInfo(String provider, String accessToken, Map<String, Object> attributes) {
+        if (OAuth2Provider.GOOGLE.getProvider().equals(provider)) {
+            return new GoogleOAuth2UserInfo(accessToken, attributes);
+        } else if (OAuth2Provider.NAVER.getProvider().equals(provider)) {
+            return new NaverOAuth2UserInfo(accessToken, attributes);
+        } else {
+            throw new OAuth2AuthenticationProcessingException(provider + "을/를 통한 로그인은 불가합니다.");
+        }
+    }
 }
